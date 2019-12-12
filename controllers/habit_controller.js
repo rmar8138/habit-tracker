7db4863e057b6f48054cf6792529e1fae2ee1e2b
rmar8138 <ragan.martinez@live.com>
2019-12-12 15:26:56 +1100

    Error handling for habit controller

--- controllers/habit_controller.js
+++ controllers/habit_controller.js
@@ -1,39 +1,59 @@
 const HabitModel = require("../database/models/habit_model");
 
 const index = async (req, res) => {
-  const habits = await HabitModel.find();
-  res.json(habits);
+  try {
+    const habits = await HabitModel.find();
+    res.json(habits);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 const show = async (req, res) => {
   const { id } = req.params;
-  const habit = await HabitModel.findById(id);
-  res.json(habit);
+  try {
+    const habit = await HabitModel.findById(id);
+    res.json(habit);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 const create = async (req, res) => {
   const { name } = req.body;
-  const habit = await HabitModel.create({ name });
-  res.json(habit);
+  try {
+    const habit = await HabitModel.create({ name });
+    res.json(habit);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 const update = async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
-  const habit = await HabitModel.findByIdAndUpdate(
-    id,
-    { name },
-    {
-      new: true
-    }
-  );
-  res.json(habit);
+  try {
+    const habit = await HabitModel.findByIdAndUpdate(
+      id,
+      { name },
+      {
+        new: true
+      }
+    );
+    res.json(habit);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 const destroy = async (req, res) => {
   const { id } = req.params;
-  const habit = await HabitModel.findByIdAndDelete(id);
-  res.json(habit);
+  try {
+    const habit = await HabitModel.findByIdAndDelete(id);
+    res.json(habit);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 module.exports = {
